503844830d5371611d27fdfab7bf7bbfe9b535fd
Jana Yassin <janaabbasyassin@gmail.com>
2023-09-15 14:12:40 +0300

    push

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -4,7 +4,7 @@ import './style.css';
 function About() {
   return (
     <div className="blue-part bg-blue-500 pt-40 pb-40 pl-60  text-white">
-      <h1 className="text-[40px] font-bold mb-6 text-left w-96 mr-60">Discover The Power Of Great Music</h1>
+      <h1 className="text-[40px] font-bold mb-6 text-left w-96 mr-60 ">Discover The Power Of Great Music</h1>
       <p className="text-[15px] mb-4 text-left w-96 mr-60">
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudan totam rem aperi.
       </p>
--- src/app/components/Features.js
+++ src/app/components/Features.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Features() {
   return (
     <div className="features-part new-page min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold mb-4 p-2">Technology Keeps Moving Forward</h1>
+      <h1 className="text-4xl font-bold mb-4 p-2 text-black">Technology Keeps Moving Forward</h1>
       <p className="semi-title text-[15px] text-center text-slate-400 mb-8 p-2">
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui offici
       </p>
@@ -12,19 +12,19 @@ function Features() {
         
             <div class="arrow-icon icon"></div>
             <div>
-                <h2 class="icon-title text-lg font-semibold mt-3">Latest Technology</h2>
+                <h2 class="icon-title text-lg font-semibold mt-3 text-black">Latest Technology</h2>
                 <p class="icon-text w-64 text-sm text-gray-500">Sed ut perspiciatis unde omnis iste natus error sit volu atem accusam.</p>
             </div>
             
             <div class="heart-icon icon"></div>
       <div>
-        <h2 className="text-lg font-semibold mt-3">Modern Design</h2>
+        <h2 className="text-lg font-semibold mt-3 text-black">Modern Design</h2>
         <p className="icon-text w-64 text-sm text-gray-500">Sed ut perspiciatis unde omnis iste natus error sit volu atem accusam.</p>
       </div>
 
       <div class="sound-icon icon"></div>
       <div>
-        <h2 className="text-lg font-semibold mt-3">Sound Clarity</h2>
+        <h2 className="text-lg font-semibold mt-3 text-black">Sound Clarity</h2>
         <p className="icon-text w-64 text-sm text-gray-500">Sed ut perspiciatis unde omnis iste natus error sit volu atem accusam.</p>
       </div>
         </div>
@@ -36,17 +36,17 @@ function Features() {
         <div class="">
             <div class="speakeer-icon icon"></div>
             <div>
-                <h2 class="text-lg font-semibold mt-3">Easy To Use</h2>
+                <h2 class="text-lg font-semibold mt-3 text-black">Easy To Use</h2>
                 <p class="icon-text w-64 text-sm text-gray-500">Sed ut perspiciatis unde omnis iste natus error sit volu atem accusam.</p>
             </div>
             <div class="layers-icon icon"></div>
       <div>
-        <h2 className="text-lg font-semibold mt-3">Layered Materials</h2>
+        <h2 className="text-lg font-semibold mt-3 text-black">Layered Materials</h2>
         <p className="icon-text w-64 text-sm text-gray-500">Sed ut perspiciatis unde omnis iste natus error sit volu atem accusam.</p>
       </div>
       <div class="wireless-icon icon"></div>
       <div>
-        <h2 className="text-lg font-semibold mt-3">Wireless Available</h2>
+        <h2 className="text-lg font-semibold mt-3 text-black">Wireless Available</h2>
         <p className="icon-text w-64 text-sm text-gray-500">Sed ut perspiciatis unde omnis iste natus error sit volu atem accusam.</p>
       </div>
         </div>
